2eff06bc513b3424b778b0512112f928cfb69c6d
Ramandeep Bhagat <ramandeepbhagat@protonmail.com>
2022-08-28 02:09:54 +0530

    updates /frontend homeComponent

--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="container">
       <img
-        src="http://laundromint.co/wp-content/uploads/2018/02/Laundro-mint-banner-3.png"
+        src="https://laundromint.co/wp-content/uploads/2018/02/Laundro-mint-banner-3.png"
         className="img-fluid"
         alt="banner image"
       ></img>
