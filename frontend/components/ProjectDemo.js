26d6084dc6b514ad6cd72f55157a8c026b3a4125
Ramandeep Bhagat <ramandeepbhagat@protonmail.com>
2022-08-30 04:38:22 +0530

    updates about page

--- frontend/components/ProjectDemo.js
+++ frontend/components/ProjectDemo.js
@@ -1,14 +1,8 @@
 import React from "react";
 
 export default function ProjectDemo() {
-  const handleOnLoad = (e) => {
-    console.log(e);
-    let g = document.getElementById("demo_gif");
-    g.style.display = "none";
-  };
-
   return (
-    <div className="container">
+    <>
       <h1>
         Project video -
         <a href="https://github.com/ramandeepbhagat/washkart" target="_blank">
@@ -16,36 +10,17 @@ export default function ProjectDemo() {
         </a>
       </h1>
       <div className="my-3">
-        <div
-          style={{
-            position: "relative",
-            paddingBottom: 56.25 + "%",
-            height: 0,
-          }}
+        <a
+          href="https://www.loom.com/share/2f5a294a7f1b45af8318233c22a75a86"
+          target="_blank"
         >
           <img
             id="demo_gif"
+            className="img-fluid"
             src="https://cdn.loom.com/sessions/thumbnails/2f5a294a7f1b45af8318233c22a75a86-with-play.gif"
           />
-
-          <iframe
-            className="iframe-video-demo"
-            src="https://www.loom.com/embed/2f5a294a7f1b45af8318233c22a75a86"
-            frameBorder="0"
-            webkitallowfullscreen="true"
-            mozallowfullscreen="true"
-            allowFullScreen={true}
-            style={{
-              position: "absolute",
-              top: 0,
-              left: 0,
-              width: 100 + "%",
-              height: 100 + "%",
-            }}
-            onLoad={handleOnLoad}
-          ></iframe>
-        </div>
+        </a>
       </div>
-    </div>
+    </>
   );
 }
